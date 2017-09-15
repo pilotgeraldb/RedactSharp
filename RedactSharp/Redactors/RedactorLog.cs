ea2b26ef5000c0f824455df0189082c66c7c2625
ralphy15 <geraldeburkholder@gmail.com>
2017-09-15 14:56:15 -0500

    removed redactorlog.cs

--- RedactSharp/Redactors/RedactorLog.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace RedactSharp.Redactors
-{
-    public static class RedactorLog
-    {
-        public static List<RedactorLogItem> Logs { get; set; } = new List<RedactorLogItem>();
-    }
-}
